c6dc1c0fbfef98a8e2e5cb76e1aea380c73668f3
jeremiah-eth <ihejierechibueze@gmail.com>
2025-11-22 16:54:08 -0500

    Implement link command logic

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -1,4 +1,7 @@
 import { Command, Flags, Args } from '@oclif/core';
+import { generateUniversalLink, generateDeepLink } from '../utils/link-generator.js';
+import { WALLET_SCHEMES } from '../utils/wallet-links.js';
+import chalk from 'chalk';
 
 export default class Link extends Command {
     static description = 'Generate deep links for WalletConnect URIs';
@@ -26,6 +29,21 @@ export default class Link extends Command {
 
     async run(): Promise<void> {
         const { args, flags } = await this.parse(Link);
-        this.log('Link command initialized. Logic to be implemented.');
+        const uri = args.uri;
+
+        if (flags.wallet) {
+            const scheme = WALLET_SCHEMES[flags.wallet.toLowerCase()];
+            if (!scheme) {
+                this.error(`Unknown wallet: ${flags.wallet}. Supported: ${Object.keys(WALLET_SCHEMES).join(', ')}`);
+            }
+            const link = generateDeepLink(uri, scheme);
+            this.log(chalk.green(`\n📱 ${flags.wallet} Deep Link:`));
+            this.log(link);
+        } else {
+            // Default to universal link
+            const link = generateUniversalLink(uri);
+            this.log(chalk.green('\n🌍 Universal Link:'));
+            this.log(link);
+        }
     }
 }
