da7879653347a5bf89020af590fd9a12b9cc2848
jeremiah-eth <ihejierechibueze@gmail.com>
2025-11-22 16:54:51 -0500

    Add QR code support to link command

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -2,13 +2,14 @@ import { Command, Flags, Args } from '@oclif/core';
 import { generateUniversalLink, generateDeepLink } from '../utils/link-generator.js';
 import { WALLET_SCHEMES } from '../utils/wallet-links.js';
 import chalk from 'chalk';
+import qrcode from 'qrcode-terminal';
 
 export default class Link extends Command {
     static description = 'Generate deep links for WalletConnect URIs';
 
     static examples = [
         '<%= config.bin %> <%= command.id %> "wc:..." --wallet metamask',
-        '<%= config.bin %> <%= command.id %> "wc:..." --universal',
+        '<%= config.bin %> <%= command.id %> "wc:..." --universal --qr',
     ];
 
     static args = {
@@ -25,25 +26,35 @@ export default class Link extends Command {
             description: 'Generate a universal link (https://app.walletconnect.org)',
             exclusive: ['wallet'],
         }),
+        qr: Flags.boolean({
+            description: 'Render QR code for the link',
+            default: false,
+        }),
     };
 
     async run(): Promise<void> {
         const { args, flags } = await this.parse(Link);
         const uri = args.uri;
+        let link: string;
 
         if (flags.wallet) {
             const scheme = WALLET_SCHEMES[flags.wallet.toLowerCase()];
             if (!scheme) {
                 this.error(`Unknown wallet: ${flags.wallet}. Supported: ${Object.keys(WALLET_SCHEMES).join(', ')}`);
             }
-            const link = generateDeepLink(uri, scheme);
+            link = generateDeepLink(uri, scheme);
             this.log(chalk.green(`\n📱 ${flags.wallet} Deep Link:`));
-            this.log(link);
         } else {
             // Default to universal link
-            const link = generateUniversalLink(uri);
+            link = generateUniversalLink(uri);
             this.log(chalk.green('\n🌍 Universal Link:'));
-            this.log(link);
+        }
+
+        this.log(link);
+
+        if (flags.qr) {
+            this.log(chalk.yellow('\n📷 QR Code:'));
+            qrcode.generate(link, { small: true });
         }
     }
 }
